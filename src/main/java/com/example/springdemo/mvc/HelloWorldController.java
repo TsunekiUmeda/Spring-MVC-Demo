fc5d469a8bda031ac8e0f72918c37fa69a3cc3c0
Tsuneki Umeda <umedat@amazon.co.jp>
2021-06-19 21:50:25 +0900

    add method using RequestParam annotation

--- src/main/java/com/example/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/example/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -30,6 +31,18 @@ public class HelloWorldController {
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(
+            @RequestParam("studentName") String theName,
+            Model model) {
+
+//        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Hey My Friend from v3! " + theName;
         model.addAttribute("message", result);
         return "helloworld";
     }

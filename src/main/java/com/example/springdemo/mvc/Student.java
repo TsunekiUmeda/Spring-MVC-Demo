3d014fcf3a3a139f917a4dbe087967e7dcc08440
Tsuneki Umeda <umedat@amazon.co.jp>
2021-06-20 14:37:00 +0900

    changed to use property file for country options

--- src/main/java/com/example/springdemo/mvc/Student.java
+++ src/main/java/com/example/springdemo/mvc/Student.java
@@ -1,22 +1,12 @@
 package com.example.springdemo.mvc;
 
-import java.util.LinkedHashMap;
-
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
-    private LinkedHashMap<String, String> countryOptions;
 
     public Student() {
-        //populate country options
-        countryOptions = new LinkedHashMap<>();
-        countryOptions.put("BR", "Brazil");
-        countryOptions.put("FR", "France");
-        countryOptions.put("DE", "Germany");
-        countryOptions.put("IN", "India");
-        countryOptions.put("US", "United States");
 
     }
 
@@ -44,7 +34,4 @@ public class Student {
         this.country = country;
     }
 
-    public LinkedHashMap<String, String> getCountryOptions() {
-        return countryOptions;
-    }
 }
--- src/main/webapp/WEB-INF/view/student-form.jsp
+++ src/main/webapp/WEB-INF/view/student-form.jsp
@@ -16,7 +16,7 @@
     <br><br>
     <form:select path="country">
 
-        <form:options items="${student.countryOptions}"/>
+        <form:options items="${theCountryOptions}"/>
 
     </form:select>
     <br><br>

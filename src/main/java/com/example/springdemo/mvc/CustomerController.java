b3f109fe9ded0f7bb9a3ef9db421acc625d6a072
Tsuneki Umeda <umedat@amazon.co.jp>
2021-06-22 18:07:59 +0900

    Add InitBinder to trim whitespace

--- src/main/java/com/example/springdemo/mvc/CustomerController.java
+++ src/main/java/com/example/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.example.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,15 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    //add an init binder ... to convert trim input string
+    //remove leading and trailing whitespace
+    //resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theMode) {
